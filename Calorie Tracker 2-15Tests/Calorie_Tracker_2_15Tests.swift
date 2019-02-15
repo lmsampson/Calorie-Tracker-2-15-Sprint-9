cd47ddbd11946f1acbb246bd935978b8ecc366ad
Lisa Sampson <lisasampson0918@gmail.com>
2019-02-15 11:16:57 -0500

    initial commit

---
+++ Calorie Tracker 2-15Tests/Calorie_Tracker_2_15Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Calorie_Tracker_2_15Tests.swift
+//  Calorie Tracker 2-15Tests
+//
+//  Created by Lisa Sampson on 2/15/19.
+//  Copyright © 2019 Lisa M Sampson. All rights reserved.
+//
+
+import XCTest
+@testable import Calorie_Tracker_2_15
+
+class Calorie_Tracker_2_15Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
