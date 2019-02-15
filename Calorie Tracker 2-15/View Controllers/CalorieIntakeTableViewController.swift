c2477b0db8adc5a5985a908b3bfa2045c44b0922
Lisa Sampson <lisasampson0918@gmail.com>
2019-02-15 12:56:11 -0500

    Set up table view delegate methods

--- Calorie Tracker 2-15/View Controllers/CalorieIntakeTableViewController.swift
+++ Calorie Tracker 2-15/View Controllers/CalorieIntakeTableViewController.swift
@@ -7,31 +7,40 @@
 //
 
 import UIKit
+import CoreData
+import SwiftChart
 
 class CalorieIntakeTableViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    // Properties and Outlets
+    
+    var intake: [Intake] = []
+    let series = ChartSeries([])
+    let formatter = DateFormatter()
+
+    @IBOutlet weak var chart: Chart!
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return intake.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "intakeCell", for: indexPath)
         
+        let calIntake = intake[indexPath.row]
+        cell.textLabel?.text = "Calories: \(calIntake.calories)"
         
+        formatter.dateFormat = "MM/dd/yyyy 'at' hh:mm aaa"
+        cell.detailTextLabel?.text = calIntake.timestamp.map({ (timestamp) -> String in
+            return formatter.string(from: timestamp)
+        })
 
         return cell
     }
-
-    
-//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        if editingStyle == .delete {
-//            tableView.deleteRows(at: [indexPath], with: .fade)
-//        }
-//    }
 }
